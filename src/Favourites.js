66b7503cf60eed004d77d6fbae82618a35120dfb
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2020-12-03 01:12:47 +0530

    fav section

--- src/Favourites.js
+++ src/Favourites.js
@@ -5,6 +5,15 @@ function Favourites() {
 
     const movies = useContext(MovieContext).favMovie;
 
+    let f =0;
+    let t = movies.map(movie => (
+        movie.fav? f++ : null
+     ))
+     if (f===0){
+         return <h1>No Favourites Yet!!</h1>
+     }
+
+
     return (
         <div> 
             <ul className="ul-list-fav">
